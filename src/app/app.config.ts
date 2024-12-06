23955b43b71537bb9d2c66193432f7e44cd0cc3b
M1roel <ppfautsch@yahoo.de>
2024-12-06 16:49:46 +0100

    connect firebase with project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"danotes-bbbbd","appId":"1:616435441931:web:b0734159aa44e1b01dc20e","storageBucket":"danotes-bbbbd.firebasestorage.app","apiKey":"AIzaSyA9YZPSDEjKD_aeYt1OR20nGUc3v5SX6YQ","authDomain":"danotes-bbbbd.firebaseapp.com","messagingSenderId":"616435441931"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
